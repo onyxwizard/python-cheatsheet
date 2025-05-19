6d1d08db35e29f2b6877510a774dc98cd63cdd82
AK <67966017+onyxwizard@users.noreply.github.com>
2025-05-19 21:45:24 +0530

    docs : Included python files that represent strings topic under python fundamentals section.

---
+++ Section_1_fundamentals/strings/backslash.py
@@ -0,0 +1,2 @@
+message = 'It\'s also a valid string'
+print(message,'To escape the quotes, you use the backslash (\).')
---
+++ Section_1_fundamentals/strings/raw_string.py
@@ -0,0 +1,4 @@
+message = r'C:\python\bin'
+output = 'C:\python\bin'
+print('\b')
+print(f'Valid way to add backslash is using raw string "r"\nMessage -> {message}\nMessage -> {output}')
