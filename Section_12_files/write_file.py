8384140a2d22bacaa982c870c60e0b174b68b8ac
AK <67966017+onyxwizard@users.noreply.github.com>
2025-05-24 11:17:49 +0530

    Updated the comments

--- Section_12_files/write_file.py
+++ Section_12_files/write_file.py
@@ -1,13 +1,13 @@
 #open a file and write it
 # use with to close file automatically
 
-# Approach 1
+# Approach 1 - Manually close the file
 lines = ['Readme', '\nHow to write text files in Python']
 file = open('sample.txt','w')
 file.writelines(lines)
 file.close()
 
-# Approach 2
+# Approach 2 - use with to Auto close the file
 words = ['\nReadme', '\nHow to write text files in Python']
 with open('sample.txt','a') as file:
     file.writelines(words)
