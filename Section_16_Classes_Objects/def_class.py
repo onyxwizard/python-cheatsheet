14aa7bd862facf52f7f97dfc10b014de1c32a785
AK <67966017+onyxwizard@users.noreply.github.com>
2025-05-25 00:36:47 +0530

    feat: create Mobile class with instance attributes
    - Define `Mobile` class
    - Add constructor with instance attributes: `name`, `os`

---
+++ Section_16_Classes_Objects/def_class.py
@@ -0,0 +1,6 @@
+class Mobile: # class Definition
+    pass
+
+mobile  = Mobile() # instance of a class
+mobile.model = "Honor" # Attribute instance that is only accessible by mobile instance
+print(mobile.model)
